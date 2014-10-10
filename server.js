42bc03c8dac67446d250772006d06217a202071c
csmpls <yes@cosmopol.is>
2014-10-10 01:34:42 +0000

    working minimal example

--- server.js
+++ server.js
@@ -1,57 +1,67 @@
+// setup
 var _ = require('underscore'),
     http = require('http'),
     path = require('path')
 var config = require('./config')
+
+var express = require('express'),
+    bodyParser = require('body-parser');
+var app = express(); app.use(bodyParser.json())
+var http = require('http').Server(app);
 
 var Sequelize = require('sequelize')
   , sequelize = new Sequelize(config.database.db_name,config.database.username,config.database.password, 
 	{port:5432, dialect: 'postgres',logging:false})
 var pg = require('pg').native;
 
+
+
+//   1    models
 var Reading = sequelize.define('Reading', {
   username: Sequelize.STRING,
   start_time: Sequelize.DATE,
   end_time: Sequelize.DATE,
-  raw_data: Sequelize.ARRAY(Sequelize.FLOAT),
+  raw_values: Sequelize.ARRAY(Sequelize.INTEGER),
   attention_esense: Sequelize.INTEGER,
   meditation_esense: Sequelize.INTEGER,
-  eeg_power: Sequelize.ARRAY(Sequelize.FLOAT),
+  eeg_power: Sequelize.ARRAY(Sequelize.INTEGER),
 }); Reading.sync()
 
-var app = require('express')();
-var http = require('http').Server(app);
 
+
+//    2    routes
 app.get('/handshake', function(req, res){
   res.json({time:new Date()} )
 });
 
-app.get('/', function(req, res){
+app.route('/')
+.get(function(req, res, next){
   res.sendFile('index.html', 
     { root: path.join(__dirname, 'public') });
-});
-
-app.post('/', function(req, res) {
-  saveData(req);
-  res.end();
-});
+})
+.post(function(req, res, next) {
+  processData(req.body)
+  res.json({status:'ok'}); //return confirmation of receipt
+})
 
 http.listen(config.port_number, function(){
   console.log('listening on ' + config.port_number);
 });
 
 
-function saveData(postRequest) {
-  var d = JSON.parse(data)
+
+//    3    data processing workflow
+function processData(d) {
   var reading = Reading.create({
-          username: 	d.username,
-          start_time:     d.start_time,
-          end_time:    	d.end_time,
-  	  signal_quality: d.signal_quality,
-          raw_data:  	d.raw_data,
-          attention_esense:  d.attention_esense,
-          meditation_esense:  d.meditation_esense,
-          eeg_power:  d.eeg_power,
-  }) .error(function(err) {
+          username:             d.username,
+          start_time:           d.start_time,
+          end_time:             d.end_time,
+  	  signal_quality:       d.signal_quality,
+          raw_values:           d.raw_values,
+          attention_esense:     d.attention_esense,
+          meditation_esense:    d.meditation_esense,
+          eeg_power:            d.eeg_power,
+  }).error(function(err) {
             console.log(err)
   }).success(function() {
             console.log(d.username)
